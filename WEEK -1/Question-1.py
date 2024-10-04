6dcbbcc23ce6e7e8c5d6b85bcee6887989cfdf52
Abdhulla Murshidh <murshidhraji2003@gmail.com>
2024-10-04 17:36:09 +0530

    done homework

---
+++ WEEK -1/Question-1.py
@@ -0,0 +1,6 @@
+#Write a program that asks for a user’s name and age and prints a message using both inputs.
+
+name = input("what is your Name : ")
+age = int(input("how old are you : "))
+
+print(f"Hello {name}, you are {age} years old." )
---
+++ WEEK -1/Question-2.py
@@ -0,0 +1,10 @@
+#Write a program that takes two numbers and prints their sum.
+
+print("we are creating a program to calculate the sum of your favourite two numbers. ")
+
+num1 = int(input("Please enter 1st Number : "))
+num2 = int(input("please eanter 2nd number : "))
+
+sum = num1+num2
+
+print(f"The sum of your two favourite numbers {num1} and {num2} is {sum}. ")
